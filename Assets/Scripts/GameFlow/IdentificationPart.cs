31f9c5a41caeb76d4884e7296b800631ed12fe95
tonyc0213 <cheng.tony123@gmail.com>
2026-01-31 21:49:49 +0800

    Some identification part

--- Assets/Scripts/GameFlow/IdentificationPart.cs
+++ Assets/Scripts/GameFlow/IdentificationPart.cs
@@ -1,10 +1,44 @@
+using System;
+using System.Collections.Generic;
 using Identification;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GameFlow
 {
     public class IdentificationPart : MonoBehaviour
     {
         public IdentificationSuspect suspectPrefab;
+
+        public RectTransform layoutRoot;
+        public List<IdentificationSuspect> suspects = new List<IdentificationSuspect>();
+
+        private void Start()
+        {
+            GenerateSuspects();
+        }
+
+        void GenerateSuspects()
+        {
+            foreach (var (hash, face) in FaceAndDrawings.singleton.drawnFaces)
+            {
+                var newSuspect = Instantiate(suspectPrefab, layoutRoot);
+                newSuspect.SetMask(hash,face);
+                newSuspect.SetCallback(OnClickSuspect);
+                suspects.Add(newSuspect);
+            }
+        }
+
+        void OnClickSuspect(long suspectHash)
+        {
+            if (suspectHash == FaceAndDrawings.singleton.suspectFaceHash[FaceAndDrawings.singleton.realCulpritIndex])
+            {
+                Debug.Log("Correct Answer!");
+            }
+            else
+            {
+                Debug.Log("Wrong Answer");
+            }
+        }
     }
 }
