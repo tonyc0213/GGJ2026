1e5784458cc72af07e29c8758d4fd4fcb70bd738
tonyc0213 <cheng.tony123@gmail.com>
2026-02-01 11:56:06 +0800

    add tutor after intro

--- Assets/Scripts/IntroScript/IntroScript.cs
+++ Assets/Scripts/IntroScript/IntroScript.cs
@@ -10,31 +10,52 @@ public class IntroScript : MonoBehaviour
     [SerializeField] public string Scene;
     [SerializeField] private GameObject Cn;
     [SerializeField] private GameObject Eng;
+    
+    [SerializeField] private GameObject CnTutor;
+    [SerializeField] private GameObject EngTutor;
 
+    [SerializeField] private GameObject Intro;
+    [SerializeField] private GameObject Tutor;
 
+    private bool showingIntro = false;
 
     void Start()
     {
+        Intro.SetActive(true);
+        Tutor.SetActive(false);
         lauguageId = PlayerPrefs.GetInt("language");
         if(lauguageId == 1)
         {
             Cn.SetActive(false);
+            CnTutor.SetActive(false);
             Eng.SetActive(true);
+            EngTutor.SetActive(true);
         }
         else
         {
             Cn.SetActive(true);
+            CnTutor.SetActive(true);
             Eng.SetActive(false);
+            EngTutor.SetActive(false);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.anyKeyDown)
         {
-            //gameObject.SetActive(false);
-            SceneManager.LoadScene(Scene);
+            if (!showingIntro)
+            {
+                Intro.SetActive(false);
+                Tutor.SetActive(true);
+                showingIntro = true;
+            }
+            else
+            {
+                //gameObject.SetActive(false);
+                SceneManager.LoadScene(Scene);
+            }
            
         }
     }
